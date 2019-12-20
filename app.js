6e3d788db745dfef1916e3a6422380293f060ad8
Jorelle Agustin <jagustin@stratpoint.com>
2019-12-20 09:29:35 +0800

    ready for deployment

--- app.js
+++ app.js
@@ -1,23 +1,43 @@
+const path = require('path');
+const fs = require('fs');
+const https = require('https');
+
 const express = require('express');
 const mongoose = require('mongoose');
+
 const bodyParser = require('body-parser');
 const usersRoutes = require('./routes/users');
 const authRoutes = require('./routes/auth');
 
 const app = express();
+const helmet = require('helmet');
+const compression = require('compression');
+const morgan = require('morgan');
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+
+
+
+app.use(helmet());
+app.use(compression());
+app.use(morgan('combined', { stream: accessLogStream }));
+
+const MONGODB_URI = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-ibwkk.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}?retryWrites=true&w=majority`;
+
+const privateKey = fs.readFileSync('server.key');
+const certificate = fs.readFileSync('server.cert');
+
+
 
 app.use(bodyParser.json());
+
 
 app.use(usersRoutes);
 app.use(authRoutes);
 
-
-
-
-
-mongoose.connect('mongodb+srv://thirdweek:196SsR9V1OvXKYxS@cluster0-ibwkk.mongodb.net/restapi?retryWrites=true&w=majority')
+mongoose.connect(MONGODB_URI)
     .then(result => {
-        app.listen(8080);
+        // https.createServer({ key:privateKey, cert: certificate }, app).listen(process.env.PORT || 8080);
+        app.listen(process.env.PORT || 8080);
     }).catch(err => {
         console.log(err);
     })
